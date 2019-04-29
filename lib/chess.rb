f1f34f065f4416e5e8b7e53654e1da8997b5da48
Tyler Jarvik <tjarvik@gmail.com>
2019-04-28 20:01:40 -0700

    most legal moves defined

--- lib/chess.rb
+++ lib/chess.rb
@@ -30,16 +30,37 @@ end
 class Piece
     attr_accessor :id
     attr_accessor :color
+    attr_accessor :ever_moved
+    attr_accessor :just_moved
 
-    def initialize(id, color)
+     def initialize(id, color)
         @id = id
         @color = color
+        @ever_moved = false
+        @just_moved = false
     end
 
     def rc_diff(from, to)
         row_diff = to[1].to_i - from[1].to_i
         col_diff = to[0].ord - from[0].ord
         return row_diff, col_diff
+    end
+
+    def path_clear?(from, to, squares)
+        return true if self.is_a?(Knight)
+        row_diff, col_diff = rc_diff(from, to)
+        row_direction = row_diff == 0 ? 0 : row_diff / row_diff.abs
+        col_direction = col_diff == 0 ? 0 : col_diff / col_diff.abs
+        row = from[1].to_i
+        col = from[0].ord - 96
+        diff = [row_diff.abs, col_diff.abs].max
+        diff.times do |n|
+            next if n == 0
+            this_row = row + n * row_direction
+            this_col = col + n * col_direction
+            return false if squares["#{(this_col + 96).chr}#{this_row}"]
+        end
+        true
     end
 end
 
@@ -48,10 +69,22 @@ class King < Piece
         self.color == "W" ? "\u2654" : "\u265A"
     end
 
+    
     def legal?(from, to, squares)
         row_diff, col_diff = rc_diff(from, to)
-        row_diff.abs <= 1 && col_diff.abs <= 1
-        ##or castles
+        return true if row_diff.abs <= 1 && col_diff.abs <= 1
+        return false if self.ever_moved
+        if row_diff == 0 && col_diff.abs == 2 #castles
+            rook_col = row_diff == 2 ? "h" : "a"
+            rook_square = "#{rook_col}#{from[1]}"
+            rook = squares[rook_square]
+            return false if rook.nil?
+            return false if rook.ever_moved
+            return false unless path_clear?(from, rook_square, squares)
+            #return false if in check in intervening squares
+            return true
+        end
+        false
     end
 
     def in_check?
@@ -116,7 +149,31 @@ class Pawn < Piece
 
     def legal?(from, to, squares)
         row_diff, col_diff = rc_diff(from, to)
-        true #####
+        direction = self.color == "W" ? 1 : -1
+        return false if col_diff.abs > 1
+        return false unless row_diff == direction || row_diff == direction * 2
+        row = from[1].to_i
+        col = from[0].ord - 96
+        if col_diff == 0 #move forward
+            return false if squares["#{from[0]}#{row + direction}"]
+            if row_diff == 2
+                return false if self.ever_moved = true
+                self.just_moved = true
+            end
+        else #capture
+            return false if row_diff != direction
+            if squares[to].nil? 
+                en_passant = squares["#{to[0]}#{from[1]}"]
+                return false if en_passant.nil?
+                return false unless en_passant.is_a?(Pawn)
+                return false if en_passant.color == self.color
+                return false unless en_passant.just_moved == true
+                en_passant = nil
+            else
+                return false if squares[to].color == self.color
+            end 
+        end
+        true
     end
 end
 
@@ -130,7 +187,7 @@ class Player
     end
 
     def in_check?
-        king_id = "#{@name[0]}K"
+
         false
     end
 end
@@ -169,7 +226,9 @@ class Game
     def take_turn
         @board.display
         #checkmate?
-        prompt_move
+        move = prompt_move
+
+        make_move(move)
         @current_player = @current_player == @white ? @black : @white
     end
 
@@ -197,20 +256,36 @@ class Game
         from = move[0..1]
         to = move[3..4]
         piece = @board.squares[from]
+        captured = @board.squares[to]
         return false if piece.nil?
-        return false unless piece.color == @current_player.color
         return false if from == to
+        return false if piece.color != @current_player.color
+        return false if captured && piece.color == captured.color
         return false if piece.legal?(from, to, @board.squares) == false
-        #is something in the way?
-        #if piece on final square, is it opponent's?
-        #if pawn, special capture rules
-        #not moving self into check
-        #special castle rules
+        return false if piece.path_clear?(from, to, @board.squares) == false
         @board.squares[from] = nil
         @board.squares[to] = piece
-        #if piece captured, also destroy it
+        #if castled, move rook too
+        if in_check##
+            #move it back
+            @board.squares[from] = piece
+            @board.squares[to] = captured
+            return false
+        end
+        ObjectSpace.each_object(Piece) do |pc|
+            pc.just_moved = false
+        end
+        piece.just_moved = true
+        piece.ever_moved = true
         true
     end
+
+    #def make_move(move)
+    #    from = move[0..1]
+    #    to = move[3..4]
+    #    piece = @board.squares[from]
+    #    
+    #end
     
     def save 
         #save file
