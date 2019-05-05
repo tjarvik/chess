8681360988dba33fd0a746d3246cbe57b657d4c7
Tyler Jarvik <tjarvik@gmail.com>
2019-05-05 10:25:08 -0700

    add computer play with random legal move

--- lib/game.rb
+++ lib/game.rb
@@ -8,15 +8,21 @@ class Game
         @board = State.new
         @current_player = "W"
         @current_player_name = "White"
+        @computer = false
     end
 
     def play
+        select_mode
+        turn_taker
+    end
+
+    def turn_taker
         @board.display
         take_turn until @game_over
     end
 
     def take_turn
-        move = prompt_move
+        move = get_move
         @board.make_move(move)
         @board.display
         toggle_player
@@ -42,23 +48,36 @@ class Game
         @current_player = @current_player == "W" ? "B" : "W"
     end
 
+    def select_mode
+        puts "Enter C to play against the computer, LOAD to load game, or press ENTER for 2-person play."
+        input = gets 
+        @computer = true if input =~ /C/i
+        load_game if input =~ /LOAD/i
+    end
+
+    def get_move
+        if @computer && @current_player == "B"
+            legals = @board.get_all_legals("B")
+            legals[rand(legals.length)]
+        else
+            prompt_move
+        end
+    end
+
     def prompt_move
-        puts "#{@current_player_name}'s move. Enter your move or type SAVE/LOAD for saved game:"
+        puts "#{@current_player_name}'s move. Enter your move or type SAVE to save game:"
         loop do
             input = gets.chomp.strip.downcase
             save_game if input =~ /save/
-            load_game if input =~ /load/
             unless input =~ /^[a-h][1-8]\-[a-h][1-8]$/
                 puts "Invalid move. Enter move in coordinate format, e.g., e2-e4"
                 next
             end
             move = alpha_to_rc(input)
             if legal_move?(move)
-                valid_move = true
                 return move
             else
                 puts "#{input} is not a legal move. Enter move:"
-                next
             end
         end
     end
@@ -93,14 +112,14 @@ class Game
         @game_over = true
         filename = "saved_games/game_1.txt"
         new_game = YAML::load(File.read(filename))
-        new_game.play
+        new_game.turn_taker
     end
 end
 
 
 
-#game = Game.new
-#game.play
+game = Game.new
+game.play
 
 
 
